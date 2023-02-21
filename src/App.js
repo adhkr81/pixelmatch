c66ed47a64bed11bdd94108f2c57a788a5dccab8
unknown <adhkr81@gmail.com>
2023-02-21 18:09:12 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,21 @@ function App() {
 
   async function fetchImages(img1, img2) {
 
-    try{
-      const jimage1 = await Jimp.read(img1);
-      const jimage2 = await Jimp.read(img2);
+
+      const jimage1 = await Jimp.read(img1).then((image) => {
+        console.log(image)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+
+      const jimage2 = await Jimp.default.read(img2).then((image) => {
+        console.log(image)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+      
       const width1 = jimage1.bitmap.width;
       const width2 = jimage2.bitmap.width;
       const height1 = jimage1.bitmap.height;
@@ -21,9 +33,7 @@ function App() {
             const numDiffPixels = pixelmatch(img1, img2, diff, 1600, 600, {threshold: 0.1});
             console.log(diff)
         }
-    } catch(error) {
-      console.log(error)
-    }
+
   }
 
 fetchImages("https://www.lg.com/us/images/Laundry_measure-D.jpg", "https://www.lg.com/us/images/d_measure.jpg")
